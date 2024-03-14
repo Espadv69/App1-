155e2ce41d58ed06c54fcb9ca20aa56520c9fca4
aitor <aitorjob69@gmail.com>
2024-03-14 10:25:43 +0100

    jsjsjs

---
+++ app1/js/js.js
@@ -0,0 +1,89 @@
+function uno () {
+    let nota1, nota2, nota3;
+    nota1 = parseFloat(prompt('ingrese la primera nota'));
+    nota2 = parseFloat(prompt('ingrese la segunda nota'));
+    nota3 = parseFloat(prompt('ingrese la tercera nota'));
+    resultado = (nota1 + nota2 + nota3) /3; 
+
+    if (resultado >= 5) {
+        document.getElementById('demo').innerHTML="aprobó la asignatura";
+    } else {
+        document.getElementById('demo').innerHTML="suspendió la asignatura";
+    }
+}
+
+function dos () {
+    let nota1, nota2, nota3;
+    nota1 = parseFloat(prompt('ingrese la primera nota'));
+    nota2 = parseFloat(prompt('ingrese la segunda nota'));
+    nota3 = parseFloat(prompt('ingrese la tercera nota'));
+    resultado = (nota1 + nota2 + nota3) /3; 
+
+    if (resultado >= 5) {
+        document.getElementById('demo2').innerHTML="aprobó la asignatura";
+    } else {
+        document.getElementById('demo2').innerHTML="suspendió la asignatura";
+    }
+}
+
+function tres () {
+    let nota1, nota2, nota3;
+    nota1 = parseFloat(prompt('ingrese la primera nota'));
+    nota2 = parseFloat(prompt('ingrese la segunda nota'));
+    nota3 = parseFloat(prompt('ingrese la tercera nota'));
+    resultado = (nota1 + nota2 + nota3) /3; 
+
+    if (resultado >= 5) {
+        document.getElementById('demo3').innerHTML="aprobó la asignatura";
+    } else {
+        document.getElementById('demo3').innerHTML="suspendió la asignatura";
+    }
+}
+
+function cuatro () {
+    let nota1, nota2, nota3;
+    nota1 = parseFloat(prompt('ingrese la primera nota'));
+    nota2 = parseFloat(prompt('ingrese la segunda nota'));
+    nota3 = parseFloat(prompt('ingrese la tercera nota'));
+    resultado = (nota1 + nota2 + nota3) /3; 
+
+    if (resultado >= 5) {
+        document.getElementById('demo4').innerHTML="aprobó la asignatura";
+    } else {
+        document.getElementById('demo4').innerHTML="suspendió la asignatura";
+    }
+}
+
+function cinco () {
+    let nota1, nota2, nota3;
+    nota1 = parseFloat(prompt('ingrese la primera nota'));
+    nota2 = parseFloat(prompt('ingrese la segunda nota'));
+    nota3 = parseFloat(prompt('ingrese la tercera nota'));
+    resultado = (nota1 + nota2 + nota3) /3; 
+
+    if (resultado >= 5) {
+        document.getElementById('demo5').innerHTML="aprobó la asignatura";
+    } else {
+        document.getElementById('demo5').innerHTML="suspendió la asignatura";
+    }
+}
+
+function seis () {
+    let nota1, nota2, nota3;
+    nota1 = parseFloat(prompt('ingrese la primera nota'));
+    nota2 = parseFloat(prompt('ingrese la segunda nota'));
+    nota3 = parseFloat(prompt('ingrese la tercera nota'));
+    resultado = (nota1 + nota2 + nota3) /3; 
+
+    if (resultado >= 5) {
+        document.getElementById('demo6').innerHTML="aprobó la asignatura";
+    } else {
+        document.getElementById('demo6').innerHTML="suspendió la asignatura";
+    }
+}
+
+
+
+
+
+//Math.trunc
