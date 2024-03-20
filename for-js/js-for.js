4de93920f23f0e4368bfc65423e9099258f69d1b
aitor <aitorjob69@gmail.com>
2024-03-20 09:22:26 +0100

    freestyle

---
+++ for-js/js-for.js
@@ -0,0 +1,126 @@
+let valores = 0;
+
+let valoresNegativos = 0;
+let valoresPositivos = 0;
+let multiplos = 0;
+let pares = 0;
+
+function press() {
+
+for(let i = 1; i <= 10; i++) {
+        valores = parseInt(prompt('ingrese los valores'));
+        if(valores < 0) {
+        valoresNegativos = valoresNegativos + 1;
+        } else if(valores > 0){
+        valoresPositivos = valoresPositivos + 1;
+        } if(valores % 15 == 0) {
+        multiplos = multiplos + 1;
+        } if(valores > 0 && valores %2 == 0) {
+        pares = pares + valores;
+        }
+    }
+
+    let div = document.querySelector('.pito');
+    div.innerHTML = `<h2>La cantidad de números negativos añadidos es: ${valoresNegativos}</h2><br><h2>La cantidad de números positivos añadidos es: ${valoresPositivos}</h2><br><h2>Los múltiplos de 15 son: ${multiplos}</h2><br><h2>La suma de los pares es: ${pares}</h2>`
+}
+
+let button = document.querySelector('.pito2');
+button.addEventListener('click', press);
+        
+
+
+// console.log("negativos: " + valoresNegativos);
+// console.log("positivos: " + valoresPositivos);
+// console.log("multiplos de 15: " +multiplos);
+// console.log("suma de pares: " + pares);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----MAÑANA
+
+// let morning;
+// let suma = 0;
+
+// for(let i = 1; i <= 5; i++) {
+//     morning = parseFloat(prompt(`introduzca la edad numero ${i}`));
+//     suma = suma + morning;
+// }
+
+// let promedio = (suma/5).toFixed(1);
+
+
+// ----TARDE
+// let afternooon;
+// let suma1 = 0;
+
+// for(let i = 1; i <= 6; i++) {
+//     afternooon = parseFloat(prompt(`introduzca la edad numero ${i}`));
+//     suma1 = suma1 + afternooon;
+// }
+
+// let promedio1 = (suma1/6).toFixed(1);
+
+// ----NOCHE
+// let eve;
+// let suma2 = 0;
+
+// for(let i = 1; i <= 11; i++) {
+//     eve = parseFloat(prompt(`introduzca la edad numero ${i}`));
+//     suma2 = suma2 + eve;
+// }
+
+// let promedio2 = (suma2/11).toFixed(1);
+
+// PROMEDIO
+// let div = document.querySelector('.pito');
+
+// div.innerHTML = `<h2>El promedio de la clase del turno de mañana es: ${promedio}</h2> <h2>El promedio de la clase del turno de tarde es: ${promedio1}</h2><h2>El promedio de la clase del turno de noche es: ${promedio2}</h2>`
+
+
+
+
+// let tabla = parseInt(prompt('INGRESAR UN VALOR DEL 1 - 10'));
+// let contar = tabla;
+// for(let i = 1; i <= 12; i++){
+//     if(tabla > 10) {
+//         alert('not found')
+//     } else {
+//         document.write(contar + ' ');
+//         contar = contar + tabla;
+//     }
+    
+// }
+
+
+// document.write('LA TABLA DEL 5:');
+// let tabla = 5;
+// for(let i = 1; i <= 10; i++) {
+//     document.write( '<br>'+ tabla);
+//     tabla = tabla + 5;
+// }
+
+
+
+// let suma = 0;
+// for(let i = 1; i <=10; i++) {
+//     let valor = parseInt(prompt('ingrese valores enteros'));
+//     if(i > 5) {
+//         suma = suma + valor;
+//     }
+// }
+// document.write('<h3>la suma de los ultimos 5 valores añadidos es: </h3>' + ' ' + suma);
+
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
